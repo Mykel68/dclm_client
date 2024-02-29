fc62f700b7b2743c00213c28116fbc54ba129976
Mykel68 <olowookeremicheal9568@gmail.com>
2024-02-29 19:24:54 +0100

    dclm report

--- src/App.js
+++ src/App.js
@@ -1,3 +1,48 @@
+// import "./App.css";
+// import React, { useState } from "react";
+// import ReportForm from "./pages/ReportForm";
+// import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+// import AdminPage from "./pages/AdminPage";
+// import ReportPage from "./pages/ReportPage";
+// // Add these lines in your JavaScript file
+// import "bootstrap/dist/css/bootstrap.min.css";
+// import "bootstrap/dist/js/bootstrap.bundle.min.js";
+// // import Edit from './pages/Edit';
+// import Login from "./pages/Login";
+// import EditReportPage from "./pages/EditReportPage";
+// import Register from "./pages/Register";
+// import PrivateRoute from "./utils/PrivateRoute";
+
+// function App() {
+//   const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+//   return (
+//     <BrowserRouter>
+//       <Routes>
+//         <Route path="/" element={<ReportForm />} />
+//         <Route
+//           path="/report"
+//           element={
+//             <PrivateRoute component={<ReportPage />} isLoggedIn={isLoggedIn} />
+//           }
+//         />
+//         <Route path="/admin" element={<AdminPage />} />
+//         <Route path="/edit/:id" element={<EditReportPage />} />
+//         <Route
+//           path="/login"
+//           element={<Login setIsLoggedIn={setIsLoggedIn} />}
+//         />
+//         <Route path="/register" element={<Register />} />
+//       </Routes>
+//     </BrowserRouter>
+//   );
+// }
+
+// export default App;
+
+// 👆👆👆👆👆👆👆👆
+// Working on authenicating user can't view admin page with url and bypassing login
+
 import "./App.css";
 import React, { useState } from "react";
 import ReportForm from "./pages/ReportForm";
@@ -11,27 +56,16 @@ import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import Login from "./pages/Login";
 import EditReportPage from "./pages/EditReportPage";
 import Register from "./pages/Register";
-import PrivateRoute from "./utils/PrivateRoute";
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<ReportForm />} />
-        <Route
-          path="/report"
-          element={
-            <PrivateRoute component={<ReportPage />} isLoggedIn={isLoggedIn} />
-          }
-        />
+        <Route path="/report" element={<ReportPage />} />
         <Route path="/admin" element={<AdminPage />} />
         <Route path="/edit/:id" element={<EditReportPage />} />
-        <Route
-          path="/login"
-          element={<Login setIsLoggedIn={setIsLoggedIn} />}
-        />
+        <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
       </Routes>
     </BrowserRouter>
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -30,7 +30,7 @@ const Login = () => {
 
     try {
       const response = await axios.post(
-        `${REACT_APP_BACKEND_URL}/auth/login`,
+        `${process.env.REACT_APP_BACKEND_URL}/auth/login`,
         formData
       );
 
