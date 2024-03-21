16bcf293455deea1c44896b56a0fcbaedd185e55
Mykel68 <olowookeremicheal9568@gmail.com>
2024-03-21 16:43:39 +0100

    starting admin dashboard

--- src/pages/auth/super_admin/Index.jsx
+++ src/pages/auth/super_admin/Index.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Link, useAsyncError } from "react-router-dom";
 import SupervisedUserCircleIcon from "@mui/icons-material/SupervisedUserCircle";
 import { Box, Paper, Typography, Stack } from "@mui/material";
-
+import TextSnippetIcon from "@mui/icons-material/TextSnippet";
 import Bar from "../../../components/Bar";
 import Container from "@mui/material/Container";
 import Card from "../../../components/Card";
@@ -12,10 +12,10 @@ const Index = () => {
   const [reportCount, setReportCount] = useState(0);
 
   useEffect(() => {
-    fetchCount();
+    fetchAdminCount();
   }, []);
 
-  const fetchCount = async () => {
+  const fetchAdminCount = async () => {
     try {
       const response = await fetch("/api/getAdminCount");
       const data = await response.json();
@@ -28,31 +28,44 @@ const Index = () => {
       console.error("Error fetching data:", error);
     }
   };
+  useEffect(() => {
+    fetchReportCount();
+  }, []);
 
+  const fetchReportCount = async () => {
+    try {
+      const response = await fetch("/api/getReportCount");
+      const data = await response.json();
+
+      console.log(data);
+
+      setAdminCount(data.adminCount);
+      setReportCount(data.reportCount);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
   return (
     <div>
       <Bar />
       <Container maxWidth="sm" sx={{ textAlign: "center", mt: 5 }}>
-        <Card
-          icon={
-            <SupervisedUserCircleIcon
-              color="primary"
-              style={{ fontSize: 150 }}
-            />
-          }
-          item={"Admin"}
-          quantity={adminCount}
-        />
-        <Card
-          icon={
-            <SupervisedUserCircleIcon
-              color="primary"
-              style={{ fontSize: 150 }}
-            />
-          }
-          item={"Report"}
-          quantity={reportCount}
-        />
+        <Stack direction="row" spacing={2}>
+          <Card
+            icon={
+              <SupervisedUserCircleIcon
+                color="primary"
+                style={{ fontSize: 150 }}
+              />
+            }
+            item={"Admin"}
+            quantity={adminCount}
+          />
+          <Card
+            icon={<TextSnippetIcon color="primary" style={{ fontSize: 150 }} />}
+            item={"Report"}
+            quantity={reportCount}
+          />
+        </Stack>
       </Container>
     </div>
   );
