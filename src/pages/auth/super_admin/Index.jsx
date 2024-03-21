ab08c718b66d536b9ee2a9c8ab2925cfefce8d24
Mykel68 <olowookeremicheal9568@gmail.com>
2024-03-21 16:18:30 +0100

    tested backend of admin count

--- src/pages/auth/super_admin/Index.jsx
+++ src/pages/auth/super_admin/Index.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Link, useAsyncError } from "react-router-dom";
 import SupervisedUserCircleIcon from "@mui/icons-material/SupervisedUserCircle";
 import { Box, Paper, Typography, Stack } from "@mui/material";
@@ -8,10 +8,26 @@ import Container from "@mui/material/Container";
 import Card from "../../../components/Card";
 
 const Index = () => {
-  const [item, setItem] = useState(
-    "admin".charAt(0).toUpperCase() + "admin".slice(1)
-  );
-  const [quantity, setQuantity] = useState(2);
+  const [adminCount, setAdminCount] = useState(0);
+  const [reportCount, setReportCount] = useState(0);
+
+  useEffect(() => {
+    fetchCount();
+  }, []);
+
+  const fetchCount = async () => {
+    try {
+      const response = await fetch("/api/getAdminCount");
+      const data = await response.json();
+
+      console.log(data);
+
+      setAdminCount(data.adminCount);
+      setReportCount(data.reportCount);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
 
   return (
     <div>
@@ -24,8 +40,18 @@ const Index = () => {
               style={{ fontSize: 150 }}
             />
           }
-          item={item}
-          quantity={quantity}
+          item={"Admin"}
+          quantity={adminCount}
+        />
+        <Card
+          icon={
+            <SupervisedUserCircleIcon
+              color="primary"
+              style={{ fontSize: 150 }}
+            />
+          }
+          item={"Report"}
+          quantity={reportCount}
         />
       </Container>
     </div>
